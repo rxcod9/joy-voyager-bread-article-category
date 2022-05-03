13aec96217c0641abb05fb7999ccfeb5d25f2620
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/ArticleCategoriesTableSeeder.php
+++ database/seeders/ArticleCategoriesTableSeeder.php
@@ -18,7 +18,7 @@ class ArticleCategoriesTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         ArticleCategory::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
